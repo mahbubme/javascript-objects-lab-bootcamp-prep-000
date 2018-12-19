03db03dd7f5dc84f17d0eb62c73c45031237f166
Mahbubur Rahman <mail@mahbub.me>
2018-12-19 16:50:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,4 +9,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
-function deleteFromObjectByKey
+function deleteFromObjectByKey(object, key) {
+  var new_object = object;
+  delete new_object[key];
+  
+  return new_object;
+}
